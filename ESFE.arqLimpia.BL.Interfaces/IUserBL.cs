a0d5b5eb023074e51b3dd33c594af3135d5bbec5
BryanGuevara <126702670+BryanGuevara@users.noreply.github.com>
2023-03-11 11:38:54 -0600

    Add files via upload

---
+++ ESFE.arqLimpia.BL.Interfaces/IUserBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ESFE.arqLimpia.BL.DTOs.UserDTOs;
+
+namespace ESFE.arqLimpia.BL.Interfaces
+{
+    public interface IUserBL
+    {
+        Task<CreateUserOutputDTO> Create(CreateUserInputDTO pUser);
+        Task<int> Update(UpdateUserDTO pUser);
+        Task<int> Delete(DeleteUserDTO pUser);
+        Task<GetByIdUserOutputDTO> GetById(GetByIdUserInputDTO pUser);
+        Task<List<SearchUserOutputDTO>> Search(SearchUserInputDTO pUser);
+    }
+}
