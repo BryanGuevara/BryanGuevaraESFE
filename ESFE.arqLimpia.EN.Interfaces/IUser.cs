7e0afd54b5445139418fd52bfe84f0a6193dcb7c
BryanGuevara <126702670+BryanGuevara@users.noreply.github.com>
2023-03-11 11:39:40 -0600

    Add files via upload

---
+++ ESFE.arqLimpia.EN.Interfaces/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ESFE.arqLimpia.Ne;
+
+namespace ESFE.arqLimpia.EN.Interfaces
+{
+    public interface IUser
+    {
+        void Create(User pUser);
+        void Update(User pUser);
+        void Delete(User pUser);
+        Task<User> GetById(User pUser);
+        Task<List<User>> Search(User pUser);
+    }
+}
