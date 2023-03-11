a7e766ac427a63440e3f72bd4d7af2e695d82d54
BryanGuevara <126702670+BryanGuevara@users.noreply.github.com>
2023-03-11 11:43:42 -0600

    Add files via upload

---
+++ ESFE.arqLimpia.Ne/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE.arqLimpia.Ne
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+    }
+}
