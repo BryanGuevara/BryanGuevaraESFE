c775d54acb87e9676a641e55f98add37623552e3
BryanGuevara <126702670+BryanGuevara@users.noreply.github.com>
2023-03-11 11:38:01 -0600

    Add files via upload

---
+++ ESFE.arqLimpia.BL/DependecyContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using ESFE.arqLimpia.BL.Interfaces;
+
+namespace ESFE.arqLimpia.BL
+{
+    public static class DependecyContainer
+    {
+        public static IServiceCollection AddBLDependecies(this IServiceCollection services)
+        {
+            services.AddTransient<IUserBL, UserBL>();
+            return services;
+        }
+    }
+}
