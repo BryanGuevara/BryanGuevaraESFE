1dd2f3bf5eae59eb9287f591bfaaed70112856c7
BryanGuevara <126702670+BryanGuevara@users.noreply.github.com>
2023-03-11 11:43:19 -0600

    Add files via upload

---
+++ ESFE.arqLimpia.loC/DependecyContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ESFE.arqLimpia.DAL;
+using ESFE.arqLimpia.BL;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+
+namespace ESFE.arqLimpia.loC
+{
+    public static class DependecyContainer
+    {
+        public static IServiceCollection AddESFEDependecies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDALDependecies(configuration);
+            services.AddBLDependecies();
+            return services;
+        }
+    }
+}
